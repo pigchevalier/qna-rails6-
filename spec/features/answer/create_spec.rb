d83b3bae784656c641080e5e1ddb04e6595f71c9
pigchevalier <nadine.boudkina@gmail.com>
2022-01-12 16:57:57 +0300

    add files to answer and question on create

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -30,6 +30,17 @@ feature 'User can create answer', %q{
     
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'create answer with attached files' do
+      fill_in 'Body', with: 'answer_text'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+      click_on 'Create'
+
+      expect(page).to have_link "rails_helper.rb"
+      expect(page).to have_link "spec_helper.rb"
+    end
   end
 
   scenario "Unauthenticated user tries to create an answer", js: true do
