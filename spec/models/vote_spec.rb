52b71dece0e3d9fac441d163ceed338b55a5dd42
pigchevalier <nadine.boudkina@gmail.com>
2022-01-21 20:38:38 +0300

    should validate_inclusion_of

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Vote, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :value }
+    it { should validate_inclusion_of(:value).in_array([1,-1]) }
     subject { FactoryBot.build(:vote) }
     it { should validate_uniqueness_of(:user).scoped_to([:voteable_id, :voteable_type]) }
   end
